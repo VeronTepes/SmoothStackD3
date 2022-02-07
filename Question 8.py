9d6d076d7852f8183a61eea2f772e176dda7889b
VeronTepes <tickye@msn.com>
2022-02-07 12:51:56 -0700

    Finished question 8

---
+++ Question 8.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  7 12:50:16 2022
+
+@author: owner
+"""
+
+for i in range(0,6):
+    if i == 3 or i == 6:
+        continue
+    else:
+        print(i, end=" ")
